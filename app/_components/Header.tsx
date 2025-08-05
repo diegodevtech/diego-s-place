fffd25b6bd6c5ac7e16d48d68bbfa2ba85e85def
diegodevtech <diegodevtechnology@gmail.com>
2025-08-05 11:14:22 -0400

    style: removes border from header

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -4,7 +4,7 @@ import Navigation from "./Navigation";
 
 function Header() {
   return (
-    <header className="border-b border-primary-900 px-8 py-5">
+    <header className="px-8 py-5 z-10 bg-gradient-to-t from-transparent to-primary-950">
       <div className="flex justify-between items-center max-w-7xl mx-auto">
         <Logo />
         <Navigation />
