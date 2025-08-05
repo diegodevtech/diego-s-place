e4a9e81bd9d0ebd6f5eb27f38dd0ee67a1845ca4
diegodevtech <diegodevtechnology@gmail.com>
2025-08-05 11:15:31 -0400

    style: recalculates main container height for responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <main className="mt-24">
+    <div className="h-[calc(100%-100px)] flex flex-col items-center justify-center">
       <Image
-        className="object-cover object-top"
+        className="object-cover"
         placeholder="blur"
         quality={80}
         src={bg}
@@ -14,17 +14,17 @@ export default function Page() {
         alt="Mountains and forests with two cabins"
       />
 
-      <div className="relative z-10 text-center">
-        <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
+      <div className="text-center relative">
+        <h1 className="text-8xl mb-10 tracking-tight bg-clip-text text-transparent bg-gradient-to-r from-accent-500 via-accent-800 to-accent-500 font-semibold animate-gradient bg-300%">
           Welcome to paradise.
         </h1>
         <Link
           href="/cabins"
-          className="bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+          className="w-fit bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
         >
           Explore luxury cabins
         </Link>
       </div>
-    </main>
+    </div>
   );
 }
