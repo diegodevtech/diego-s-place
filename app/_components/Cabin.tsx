9f934c58bb67c2cbd17d6fcf63cbf8e0501b2aef
diegodevtech <diegodevtechnology@gmail.com>
2025-08-13 16:56:06 -0400

    refactor(Cabin): refactores Cabin component for more responsiveness

--- app/_components/Cabin.tsx
+++ app/_components/Cabin.tsx
@@ -9,7 +9,7 @@ export default function Cabin({ cabin }: { cabin: CabinType }) {
     cabin;
 
   return (
-    <div className="md:grid md:grid-cols-[3fr_4fr] md:gap-20 border border-primary-800 md:py-3 md:px-10 md:mb-24">
+    <div className="md:grid md:grid-cols-[3fr_4fr] md:gap-20 border border-primary-800 md:py-3 md:px-10 md:mb-24 mb-12 px-3">
       <div className="relative scale-[1.15] -translate-x-3 hidden md:block">
         <Image
           src={image ?? "/placeholder.png"}
@@ -18,11 +18,11 @@ export default function Cabin({ cabin }: { cabin: CabinType }) {
           className="object-cover"
         />
       </div>
-      <div className="relative md:hidden">
+      <div className="md:hidden mb-4 relative scale-[1.04] mt-2">
         <Image
           src={image ?? "/placeholder.png"}
           alt={`Cabin ${name}`}
-          width={400}
+          width={700}
           height={100}
           className="object-cover"
         />
@@ -31,8 +31,8 @@ export default function Cabin({ cabin }: { cabin: CabinType }) {
       <div>
         <h3
           className="
-        text-accent-100 font-black md:text-7xl md:mb-5 md:translate-x-[-254px] bg-primary-950 md:p-6 md:pb-1 md:w-[150%]
-
+        text-accent-100 font-black md:text-7xl mb-5 md:translate-x-[-254px] bg-primary-950 md:p-6 md:pb-1 md:w-[150%]
+          text-4xl pt-4
         "
         >
           Cabin {name}
@@ -42,7 +42,7 @@ export default function Cabin({ cabin }: { cabin: CabinType }) {
           <TextExpander>{description ?? ""}</TextExpander>
         </p>
 
-        {/* <ul className="flex flex-col gap-4 mb-7">
+        <ul className="flex flex-col gap-4 mb-7">
           <li className="flex gap-3 items-center">
             <UsersIcon className="h-5 w-5 text-primary-600" />
             <span className="text-lg">
@@ -62,7 +62,7 @@ export default function Cabin({ cabin }: { cabin: CabinType }) {
               Privacy <span className="font-bold">100%</span> guaranteed
             </span>
           </li>
-        </ul> */}
+        </ul>
       </div>
     </div>
   );
