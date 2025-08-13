55e52bc83ae52bcb83783af2303a3f615188d19b
diegodevtech <diegodevtechnology@gmail.com>
2025-08-13 15:18:14 -0400

    feat: creates one more type: CabinType

--- app/_types/index.ts
+++ app/_types/index.ts
@@ -1,1 +1,10 @@
 export type FilterTypes = "all" | "small" | "medium" | "large";
+export type CabinType = {
+  id: number;
+  name: string | null;
+  maxCapacity: number | null;
+  regularPrice: number | null;
+  discount: number | null;
+  image: string | null;
+  description: string | null;
+};
