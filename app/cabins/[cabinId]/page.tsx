ddb5b38ab77d232aa5459c7961e422072fc2fd73
diegodevtech <diegodevtechnology@gmail.com>
2025-08-18 15:50:21 -0400

    refactor: adds responsiveness to call to action text

--- app/cabins/[cabinId]/page.tsx
+++ app/cabins/[cabinId]/page.tsx
@@ -38,14 +38,14 @@ export default async function Page({ params }: { params: { cabinId: number } }) 
     // <div className="border-2">
     <div className="max-w-6xl mx-auto mt-8">
       <Cabin cabin={cabin} />
-      {/* <div>
-        <h2 className="text-5xl font-semibold text-center mb-10 text-accent-400">
+      <div>
+        <h2 className="text-3xl md:text-5xl font-semibold text-center mb-10 text-accent-400">
           Reserve {cabin.name} today. Pay on arrival.
         </h2>
-        <Suspense fallback={<Spinner />} key={cabin.id}>
+        {/* <Suspense fallback={<Spinner />} key={cabin.id}>
           <Reservation cabin={cabin} />
-        </Suspense>
-      </div> */}
+        </Suspense> */}
+      </div>
     </div>
   );
 }
