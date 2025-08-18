8db22805b276342034bbae5bae59a685c272f2e8
diegodevtech <diegodevtechnology@gmail.com>
2025-08-18 15:53:11 -0400

    feat: adds getBookedDatesByCabinId function to data-service

--- app/_service/data-service.ts
+++ app/_service/data-service.ts
@@ -1,5 +1,6 @@
 import { notFound } from "next/navigation";
 import { supabase } from "./supabase";
+import { eachDayOfInterval } from "date-fns";
 
 export const getCabins = async function () {
   const { data, error } = await supabase
@@ -31,6 +32,36 @@ export async function getCabin(id: number) {
   }
 
   return data;
+}
+
+export async function getBookedDatesByCabinId(cabinId: number) {
+  let today: Date | string = new Date();
+  today.setUTCHours(0, 0, 0, 0);
+  today = today.toISOString();
+
+  // Getting all bookings
+  const { data, error } = await supabase
+    .from("bookings")
+    .select("*")
+    .eq("cabinId", cabinId)
+    .or(`startDate.gte.${today},status.eq.checked-in`);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Bookings could not get loaded");
+  }
+
+  // Converting to actual dates to be displayed in the date picker
+  const bookedDates = data
+    .map((booking) => {
+      return eachDayOfInterval({
+        start: new Date(booking.startDate!),
+        end: new Date(booking.endDate!),
+      });
+    })
+    .flat();
+
+  return bookedDates;
 }
 
 export async function getSettings() {
