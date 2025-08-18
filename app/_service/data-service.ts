c5990ab73572c750911980b4e501ac1ca7e26781
diegodevtech <diegodevtechnology@gmail.com>
2025-08-18 15:49:52 -0400

    feat: adds getSettings function to data-service

--- app/_service/data-service.ts
+++ app/_service/data-service.ts
@@ -32,3 +32,14 @@ export async function getCabin(id: number) {
 
   return data;
 }
+
+export async function getSettings() {
+  const { data, error } = await supabase.from('settings').select('*').single();
+
+  if (error) {
+    console.error(error);
+    throw new Error('Settings could not be loaded');
+  }
+
+  return data;
+}
