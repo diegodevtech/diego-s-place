9d4b0314e88a03cb26ffc714fc539981d1a59869
diegodevtech <diegodevtechnology@gmail.com>
2025-08-19 10:36:37 -0400

    refactor: changes reservation page

--- app/_components/Reservation.tsx
+++ app/_components/Reservation.tsx
@@ -3,6 +3,7 @@ import { getBookedDatesByCabinId, getSettings } from "../_service/data-service";
 import { CabinType } from "../_types";
 import DateSelector from "./DateSelector";
 import LoginMessage from "./LoginMessage";
+import ReservationForm from "./ReservationForm";
 
 export default async function Reservation({ cabin }: { cabin: CabinType }) {
   const [settings, bookedDates] = await Promise.all([
@@ -13,19 +14,18 @@ export default async function Reservation({ cabin }: { cabin: CabinType }) {
   const session = await auth();
 
   return (
-    <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
+    <div className="md:grid md:grid-cols-2 border border-primary-800 min-h-[400px] md:mb-0 mb-8">
       {/* <div className="grid grid-cols-[1.5fr_1fr] border border-primary-800 min-h-[400px]"> */}
       <DateSelector
         cabin={cabin}
         bookedDates={bookedDates}
         settings={settings}
       />
-      {session?.user ? (
-        // <ReservationForm cabin={cabin} user={session.user} />
-        <p>a</p>
+      {/* {session?.user ? (
+        <ReservationForm cabin={cabin} user={session.user} />
       ) : (
         <LoginMessage />
-      )}
+      )} */}
     </div>
   );
 }
